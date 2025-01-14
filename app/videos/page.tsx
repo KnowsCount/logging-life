afb00576cdbe75e67cbe8c7323019066a601c54f
KnowsCount <knowscount@gmail.com>
2025-01-15 00:45:26 +0800

    update video links

--- app/videos/page.tsx
+++ app/videos/page.tsx
@@ -13,7 +13,10 @@ export default function Page() {
       <a href="https://youtu.be/hFx4M6Ud94c">https://youtu.be/hFx4M6Ud94c</a>
       <br /><br /><br /><br />
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Inkwell Poetry Open Mic Performance</h1>
-      <a href="https://youtu.be/hFx4M6Ud94c">https://youtu.be/hFx4M6Ud94c</a>
+      <a href="https://youtu.be/EUY97xaDAnU">https://youtu.be/EUY97xaDAnU</a>
+      <br /><br /><br /><br />
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">TEDx Speech on using comedy to make the best of failures</h1>
+      <a href="https://youtu.be/xxlUBvMqWdY">https://youtu.be/xxlUBvMqWdY</a>
     </section>
   )
 }
